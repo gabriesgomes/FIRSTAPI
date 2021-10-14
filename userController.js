4800a8d032b0303b5b8ec39f19a8e474ea29af60
gabriesgomes <55770651+gabriesgomes@users.noreply.github.com>
2021-10-13 23:48:32 -0300

    Delete userController.js
    Usado para teste

--- userController.js
+++
@@ -1,8 +0,0 @@
-const users = require('../mocks/users');
-
-module.exports = {
-  listUsers(request, response) {
-    response.writeHead(200, {'Content-Type': 'apllication/json'});
-    response.end(JSON.stringify(users));
-  }  
-};
